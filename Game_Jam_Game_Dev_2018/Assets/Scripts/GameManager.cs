9833cf84ec48c28bc5cb62d7297f4bb7d1b85fd9
Minh Cong Ho <ho.cong.minh1996@gmail.com>
2018-04-27 14:30:07 -0400

    Add Project Folder

---
+++ Game_Jam_Game_Dev_2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            //Checks to see if manager exists and creates an instance if it doesn't exist
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+
+
+
+            }
+            return _instance;
+        }
+
+    }
+
+    //Uses Accessors to help implement global accessibility for important variables that must be acccessed accross multiple objects/scripts
+    public int Score { get; set; }
+    public float pXSpeed { get; set; }
+    public bool PlayerSpotted { get; set; }
+    public int pHealth { get; set; }
+   
+
+	
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+}
+
---
+++ Game_Jam_Game_Dev_2018/Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myRigidbody;
+    public float xSpeed = .4f;
+    //public Transform firepoint;
+  //  public GameObject pProjectile;
+   // public int sliderPass;
+   // public Slider selfHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        GameManager.Instance.pHealth = 50;
+        //sliderPass = GameManager.Instance.pHealth;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(myRigidbody.velocity.y);
+
+        //selfHealth.value = sliderPass;
+
+        if (GameManager.Instance.pHealth == 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //        if (Input.GetKey(KeyCode.RightArrow))
+        //        {
+        //           myRigidbody.velocity = new Vector2(xSpeed, myRigidbody.velocity.y);
+        //            
+        //        }
+        //
+        //       if (Input.GetKey(KeyCode.LeftArrow)) 
+        //           {
+
+        //            myRigidbody.velocity = new Vector2(-xSpeed, myRigidbody.velocity.y);
+
+        //            } 
+        //        else
+        //      {
+        //            myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
+
+        //    }
+
+
+        //Basic Movement for player using Rigidbody Velocity
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        myRigidbody.velocity = movement * xSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            myRigidbody.velocity = (movement * xSpeed) / 2;
+        }
+      //  if (Input.GetKey(KeyCode.Space))
+        //{   //Creates a projectile object (from inspector specified prefab at an inspector specified location)
+          //  Instantiate(pProjectile, firepoint.position, firepoint.rotation);
+
+       /// }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D CollisionInfo)
+    {
+        //if (CollisionInfo.gameObject.tag == "EnemyBullet")
+        //{
+            //References Global Player health to lower health every time player is hit while keeping value accessible to all other scripts
+          //  GameManager.Instance.pHealth = (GameManager.Instance.pHealth - 1);
+           // Debug.Log(GameManager.Instance.pHealth);
+            //sliderPass = GameManager.Instance.pHealth;
+
+
+        //}
+    }
+
+
+}
